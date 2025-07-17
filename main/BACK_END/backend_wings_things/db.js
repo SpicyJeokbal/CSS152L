732ece78d72f0ea6b46b071ee8c3b24d2f707f97
SpicyJeokbal <arvin.liel.obayan2904@gmail.com>
2025-07-17 11:02:49 +0800

    updated backend

--- main/BACK_END/backend_wings_things/db.js
+++ main/BACK_END/backend_wings_things/db.js
@@ -1,12 +1,51 @@
-// db.js
+/* const { Pool } = require('pg');
+
+const pool = new Pool({
+  user: 'postgres.zpmpocivwucsjuzkgmky',
+  host: 'aws-0-ap-southeast-1.pooler.supabase.com',
+  database: 'postgres',
+  password: 'QaZWsX2904',
+  port: 6543,
+  ssl: { rejectUnauthorized: false }
+});
+
+module.exports = pool; */
+
+
 const { Pool } = require('pg');
 
 const pool = new Pool({
-  user: 'postgres',
-  host: 'localhost',
-  database: 'postgres',
-  password: 'wingsnthings',
-  port: 5432,
+  host: "aws-0-ap-southeast-1.pooler.supabase.com",
+  port: 5432, //5432
+  database: "postgres",
+  user: "postgres.heceotsazrqtejxmbsux",
+  pool_mode: "session",
+  password: "9QOc3onAnN!",
+  ssl: {
+    rejectUnauthorized: false  // required by Supabase
+  }
 });
 
-module.exports = pool;
+module.exports = pool
+
+
+
+
+
+/* postgresql://postgres.zouskdlpzpyqlpzumqyn:[PoLsD2904]@aws-0-ap-southeast-1.pooler.supabase.com:6543/postgres */
+
+/* postgresql://postgres.zpmpocivwucsjuzkgmky:[YOUR-PASSWORD]@aws-0-ap-southeast-1.pooler.supabase.com:6543/postgres */
+
+
+/* const { Pool } = require('pg');
+
+const pool = new Pool({
+  user: 'postgres',
+  host: 'db.zpmpocivwucsjuzkgmky.supabase.co',
+  database: 'postgres',
+  password: 'QaZWsX2904',
+  port: 5432,
+  ssl: { rejectUnauthorized: false }
+});
+
+module.exports = pool; */
